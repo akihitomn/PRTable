afa96986face7290cb14f452c141ff68a14ed349
tadashi nakajima <ec002068@yahoo.co.jp>
2017-07-24 14:09:53 +0900

    Add-image-upload-function-by-S3

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, "PRTable"
 set :repo_url, "git@github.com:tadasi/PRTable.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
