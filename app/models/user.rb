61f01075859ded3ca05fc3ce5a87368cba1e1878
tadashi nakajima <ec002068@yahoo.co.jp>
2017-08-03 15:20:30 +0900

    Edit user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-  belongs_to :company
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true
+  belongs_to :company, optional: true
+  # accepts_nested_attributes_for :companies
   has_many :stories
   has_many :messages
 end
