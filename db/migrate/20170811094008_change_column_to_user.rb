b646c1b93199996d64be72a0beb0b0f461ce3b98
tadashi nakajima <ec002068@yahoo.co.jp>
2017-08-11 18:50:14 +0900

    Add company_id to users_table

--- db/migrate/20170811094008_change_column_to_user.rb
+++ db/migrate/20170811094008_change_column_to_user.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :company_id, :integer, index: true, foreign_key: true
+    add_column :users, :company_id, :integer, foreign_key: true
   end
 end
