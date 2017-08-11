7b300468322b1a50738b3ffb05827e9fdac67398
tadashi nakajima <ec002068@yahoo.co.jp>
2017-08-11 15:18:14 +0900

    Companies#index change

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,34 +1,36 @@
 class CompaniesController < ApplicationController
-  # before_action :set_company, only: [:index]
-  # before_action :set_company, only: [:show, :edit, :update, :destroy]
+  before_action :set_company, only: [:index, :edit, :update]
 
-  # GET /companies
-  # GET /companies.json
   def index
-    # @companies = Company.all
+    @stories = Story.all.page(params[:page]).per(15).order('created_at DESC')
   end
 
-  # GET /companies/1
-  # GET /companies/1.json
   def show
   end
 
-  # GET /companies/new
   def new
     @company = Company.new
   end
 
-  # GET /companies/1/edit
   def edit
   end
 
-  # POST /companies
-  # POST /companies.json
-  def create
-    # @company = Company.new(company_params)
+  def update
+    if @company.update(company_params)
+    redirect_to company_stories_path(@company)
+  end
+end
+
+def create
+  @company = Company.new(company_params)
+  @company.users << current_user
+  if @company.save
+    redirect_to company_stories_path(current_user)
+  else
+    render :new
 
     # respond_to do |format|
-    #   if @company.save
+      # if @company.save
     #     format.html { redirect_to @company, notice: 'Company was successfully created.' }
     #     format.json { render :show, status: :created, location: @company }
     #   else
@@ -37,24 +39,9 @@ class CompaniesController < ApplicationController
     #   end
     # end
   end
+end
 
-  # PATCH/PUT /companies/1
-  # PATCH/PUT /companies/1.json
-  def update
-    # respond_to do |format|
-    #   if @company.update(company_params)
-    #     format.html { redirect_to @company, notice: 'Company was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @company }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @company.errors, status: :unprocessable_entity }
-    #   end
-    # end
-  end
-
-  # DELETE /companies/1
-  # DELETE /companies/1.json
-  def destroy
+def destroy
     # @company.destroy
     # respond_to do |format|
     #   format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
@@ -64,12 +51,13 @@ class CompaniesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-  # def set_company
-  #   @company = Company.find(params[:id])
-  # end
+    def set_company
+      @company = Company.find_by(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    # def company_params
-    #   params.require(:company).permit(:name)
-    # end
-end
+    def company_params
+      params.require(:company).permit(:name, user_ids: [])
+      # .merge(user_id: current_user.id)
+    end
+  end
